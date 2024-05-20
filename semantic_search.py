e94ff771592c10e99aa82e794159514bf4f10a60
muhammadmughees <muhammad.mughees@check24.de>
2024-05-21 00:23:57 +0200

    adding the Machine learning part

---
+++ semantic_search.py
@@ -0,0 +1,11 @@
+# Semantic Search:
+# 1. take documents and convert them into vectors (text embeddings)
+# 2. take a search query and convert it into a vector
+# 3. compare the vector of the search query to the vectors of the documents (to find the closest documents to eachother)
+# it is called semantic search because the meaning of the query is taken into account
+
+import polars as pl
+from sentence_transformers import SentenceTransformer, util
+from sklearn.metrics import DistanceMetric
+import numpy as np
+import matplotlib.pyplot as plt
